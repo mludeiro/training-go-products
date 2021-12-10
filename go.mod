52001c171aed1933d083bfc008ff39db9e153d7d
mludeiro <mludeiro@github.com>
2021-12-10 12:11:07 -0300

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module training-go-products
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Soy un solitario y triste proyecto de go, ojala algun dia algun dev me programe")
+}
